5ba3ffc678f5598d48f5418c0f539b39daa3d8f3
tdmask <tdmask@gmail.com>
2017-08-15 18:53:33 +0300

    Start Task05

---
+++ src/task05/task0501/task050101/MyBomb.java
@@ -0,0 +1,17 @@
+package task05.task0501.task050101;
+
+
+public class MyBomb extends Thread {
+    public void run() {
+        for (int i = 10; i>=0; i--) {
+            System.out.println(i);
+            try {
+                sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println("Bomb");
+    }
+
+}
